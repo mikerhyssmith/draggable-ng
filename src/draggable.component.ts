c08e8fd91224e1926df59309916c809765735b16
Mike Smith <mikerhyssmith@googlemail.com>
2018-01-28 18:56:19 +0000

    Some more initial project setup

---
+++ src/draggable.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'ngx-draggable',
+    templateUrl: 'draggable.component.html'
+})
+
+export class DraggableComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
